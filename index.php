e38ac820db9c9ef0bf8de00a3e63fb90d406181b
sheronfdo <jamithsheron5@gmail.com>
2022-05-15 23:54:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css
+    <link rel="stylesheet" href="css/style.css" type="text/css">
     <title>Hello, world!</title>
   </head>
   <body>
@@ -26,5 +26,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
     </body>
-    
+
 </html>
