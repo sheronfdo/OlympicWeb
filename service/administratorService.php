98929c8cc0c23573f0f16616375bd74a4f875d5c
sheronfdo <jamithsheron5@gmail.com>
2022-05-16 18:09:34 +0530

    implementation complete of all service classes

--- service/administratorService.php
+++ service/administratorService.php
@@ -1,5 +1,30 @@
 <?php
 require_once('model/adminstrator.php');
-class AdministratorService extends Administrator{
-    
+require_once('database/dbconnect.php');
+class AdministratorService extends Administrator
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new dbConnect();
+    }
+
+    public function insertAdministrator()
+    {
+        $query = "INSERT INTO `administrator`(`userName`, `email`, `password`) VALUES ('" . $this->getUsername() . "','" . $this->getEmail() . "','" . $this->getPassword() . "')";
+        $this->db->insertIntoDb($query);
+    }
+
+    public function updateAdministrator()
+    {
+        $query = "UPDATE `administrator` SET `userName`='" . $this->getUsername() . "',`email`='" . $this->getEmail() . "',`password`='" . $this->getPassword() . "' WHERE `id`= '" . $this->getId() . "'";
+        $this->db->insertIntoDb($query);
+    }
+
+    public function deleteAdministrator()
+    {
+        $query = "UPDATE `administrator` SET `status`=0 WHERE `id`= '" . $this->getId() . "'";
+        $this->db->insertIntoDb($query);
+    }
 }
--- service/sponsorContentService.php
+++ service/sponsorContentService.php
@@ -1,6 +1,30 @@
 <?php
 require_once('model/sponsorContent.php');
+require_once('database/dbconnect.php');
 class SponsorContentService extends SponsorContent
 {
-    
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new dbConnect();
+    }
+
+    public function insertContent()
+    {
+        $query = "INSERT INTO `sponsoredcontent`(`description`, `srcLink`, `imageName`, `shownLocation`) VALUES ('" . $this->getDescription() . "','" . $this->getSourceLink() . "','" . $this->getImageName() . "','" . $this->getShownLocation() . "')";
+        $this->db->insertIntoDb($query);
+    }
+
+    public function updateContent()
+    {
+        $query = "UPDATE `sponsoredcontent` SET `description`='" . $this->getDescription() . "',`srcLink`='" . $this->getSourceLink() . "',`imageName`='" . $this->getImageName() . "',`shownLocation`='" . $this->getShownLocation() . "' WHERE `id`='" . $this->getId() . "'";
+        $this->db->insertIntoDb($query);
+    }
+
+    public function deleteContent()
+    {
+        $query = "UPDATE `sponsoredcontent` SET `status`=0 WHERE `id`='" . $this->getId() . "'";
+        $this->db->insertIntoDb($query);
+    }
 }
