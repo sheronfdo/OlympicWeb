86f4b6f62a444ea849b70d00e68fe9dd161dd927
sheronfdo <jamithsheron5@gmail.com>
2022-05-17 23:40:01 +0530

    Update sidebar.php

--- common/sidebar.php
+++ common/sidebar.php
@@ -105,7 +105,7 @@
                             </li>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
-                                    <i class="nav-icon fa fa-plus-square"></i>
+                                    <i class="nav-icon fa fa-users" aria-hidden="true"></i>
                                     <p>
                                         Administrator Manage
                                         <i class="right fas fa-angle-left"></i>
