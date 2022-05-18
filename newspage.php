80542e3d045d7c2983d50c2cbfc3c0f7556528c2
sheronfdo <jamithsheron5@gmail.com>
2022-05-18 18:56:59 +0530

    make news page

---
+++ newspage.php
@@ -0,0 +1,63 @@
+<?php require_once('database/dbconnect.php'); ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/news.css">
+</head>
+
+<body>
+    <?php include('common/header.php'); ?>
+    <div class="blog">
+        <?php
+        $db = new dbConnect();
+        $query = "select * from news where status='1'";
+        $result = $db->getfromdb($query);
+        if ($rows = mysqli_num_rows($result)) {
+            $i=0;
+            while ($row = mysqli_fetch_array($result)) {
+
+
+                echo '<div class="card post" style="width: 20rem;">
+            <div class="card-body"><h5 class="card-title">' . $row['title'] . '</h5></div>
+            <img src="images/' . $row['imageName'] . '" class="card-img-top" alt="event image">
+            <div class="card-body">
+              <p class="card-text">' . substr($row['discription'], 0, 60) . '...</p>
+            </div>
+            </div>';
+
+            
+            }
+        }
+        
+        ?>
+
+    </div>
+
+
+
+    <?php include('common/footer.php'); ?>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="js/script.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
