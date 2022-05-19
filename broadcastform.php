4b13666ce696bf107e370fbe0173051b7fe14e35
sheronfdo <jamithsheron5@gmail.com>
2022-05-19 14:29:09 +0530

    update boradcast UIs

--- broadcastform.php
+++ broadcastform.php
@@ -47,12 +47,15 @@
                 <!-- form start -->
                 <form action="eventform.php" method="POST" enctype="multipart/form-data">
                     <div class="card-body">
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+                        <div class="form-group">
+                            <label for="Title">Select Event</label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
                         <div class="form-group">
                             <label for="Title">Source Link</label>
                             <input type="text" name="srcLink" class="form-control" placeholder="Enter Source Link">
