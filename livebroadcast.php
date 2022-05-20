dd4568c0556f8111d24e7955d0437e487c2cd3f9
sheronfdo <jamithsheron5@gmail.com>
2022-05-20 14:20:55 +0530

    live broadcast page

--- livebroadcast.php
+++ livebroadcast.php
@@ -1,45 +1,54 @@
-<html>
+<?php require_once('database/dbconnect.php'); ?>
+
+<!doctype html>
+<html lang="en">
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/footer.css">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/brod.css">
 </head>
 
 <body>
-  <?php
-  require 'common/header.php';
-  ?>
-  sdfsd<br>
-  sdfsdsdf<br>
-  sdsdvf<br>
-  sd<br>
-  sdfsds<br>
-  dfsdf<br>
-  dfsdfsdf<br>
-  spl_autoload<br>_functionssdf<br>
-  sdfsdsdfsd<br>
-  shm_detachfsdf<br>
-  sdfsd<br>
-  sdfsd<br>
-  sdfsd<br>
-  sdfsd<br>
-  sdfsd<br>
-  elseif<br>
-  set_file_buffer<br>
-  spl_autoload_functions<br>
-  exif_read_data<br>
-  stream_get_filters<br>
-  elseif<br>
-  endforse<br>
-  fcloses<br>
-  sdsdsdc<br>
-  sdsdvf<br>
-  <?php
-  require 'common/footer.php';
-  ?>
+    <?php include('common/header.php'); ?>
+    <div class="brod col-12">
+        <?php
+        $db = new dbConnect();
+        $query = "select * from broadcast where status='1' ORDER BY id DESC";
+        $result = $db->getfromdb($query);
+        $srcLink=null;
+        if ($rows = mysqli_num_rows($result)) {
+            $i = 0;
+            if ($row = mysqli_fetch_array($result)) {
+                $srcLink=$row['videoSrc'];
+            }
+        }
+        $converturl = str_replace("watch?v=","embed/", $srcLink);
+        echo '<iframe class="liveBrod" src="'.$converturl.'" allow="accelerometer; autoplay;" frameborder="0" allowfullscreen></iframe>';
+        ?>
+    </div>
+
+
+
+    <?php include('common/footer.php'); ?>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="js/script.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
 </body>
 
 </html>
